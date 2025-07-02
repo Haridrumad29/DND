8599efabee39b40b357bfb5f465a55fc6f83f409
Haridrumad29 <jhalaharidrumad29@gmail.com>
2025-07-02 22:02:52 +0530

    Pattern

--- sorting.cpp
+++ sorting.cpp
@@ -122,74 +122,74 @@
 //                                       INSERTION SORT
 
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-void insertionSort(int arr[], int n) {
-    for (int i = 1; i < n; i++) {
-        int key = arr[i];
-        int j = i - 1;
+// void insertionSort(int arr[], int n) {
+//     for (int i = 1; i < n; i++) {
+//         int key = arr[i];
+//         int j = i - 1;
 
         
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
+//         while (j >= 0 && arr[j] > key) {
+//             arr[j + 1] = arr[j];
+//             j--;
+//         }
 
-        arr[j + 1] = key;
-    }
+//         arr[j + 1] = key;
+//     }
     
-}
+// }
 
 
 //                 selection sort
 
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
 
-void selectionSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        int minIndex = i;
-
-        
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
-        }
+// void selectionSort(int arr[], int n) {
+//     for (int i = 0; i < n - 1; i++) {
+//         int minIndex = i;
 
         
-        if (minIndex != i) {
-            swap(arr[i], arr[minIndex]);
-        }
-    }
-}
+//         for (int j = i + 1; j < n; j++) {
+//             if (arr[j] < arr[minIndex]) {
+//                 minIndex = j;
+//             }
+//         }
+
+        
+//         if (minIndex != i) {
+//             swap(arr[i], arr[minIndex]);
+//         }
+//     }
+// }
 
 
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
+// void printArray(int arr[], int n) {
+//     for (int i = 0; i < n; i++) {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+// }
 
 
-int main() {
-    int arr[] = {29, 10, 14, 37, 13};
-    int n = sizeof(arr) / sizeof(arr[0]);
+// int main() {
+//     int arr[] = {29, 10, 14, 37, 13};
+//     int n = sizeof(arr) / sizeof(arr[0]);
 
-    cout << "Original array: ";
-    printArray(arr, n);
+//     cout << "Original array: ";
+//     printArray(arr, n);
 
-    selectionSort(arr, n);
+//     selectionSort(arr, n);
 
-    cout << "Sorted array: ";
-    printArray(arr, n);
+//     cout << "Sorted array: ";
+//     printArray(arr, n);
 
-    return 0;
-}
+//     return 0;
+// }
 
 
 
