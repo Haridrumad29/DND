d96b748afa95cd423dc8a4f77799a06944ab0b55
Haridrumad29 <jhalaharidrumad29@gmail.com>
2025-07-02 22:36:30 +0530

    pattern

--- pattern.cpp
+++ pattern.cpp
@@ -4,12 +4,20 @@ using namespace std;
 int main()
 {
     int rows;
-    cout<<"Enter the number of rows:";
-    cin>>rows;
+    cout << "Enter the number of rows:";
+    cin >> rows;
+
     for (int i = 1; i <= rows; i++)
-    {for (int j = 1; j <= i; j++)
-        {cout << "* ";
-        }cout << endl;
+    {
+        for (int k = 1; k <= rows-i; k++)
+        {
+            cout <<" ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout <<"* ";
+        }
+        cout << endl;
     }
     return 0;
 }
