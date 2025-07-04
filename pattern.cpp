674b049c9445ef25501b29d116f569e8690c3325
Haridrumad29 <jhalaharidrumad29@gmail.com>
2025-07-04 21:07:31 +0530

    Diamond Print

--- pattern.cpp
+++ pattern.cpp
@@ -1,23 +1,69 @@
+//                                        Pyramid
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int rows;
+//     cout << "Enter the number of rows:";
+//     cin >> rows;
+
+//     for (int i = 1; i <= rows; i++)
+//     {
+//         for (int k = 1; k <= rows-i; k++)
+//         {
+//             cout <<" ";
+//         }
+//         for (int j = 1; j <= i; j++)
+//         {
+//             cout <<"* ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+//                                             Half Diamond
+
+
+
+
 #include <iostream>
 using namespace std;
 
-int main()
+void printRotatedTriangle(int n)
 {
-    int rows;
-    cout << "Enter the number of rows:";
-    cin >> rows;
+   
+    for (int i = 1; i <= n; i++) {
 
-    for (int i = 1; i <= rows; i++)
-    {
-        for (int k = 1; k <= rows-i; k++)
-        {
-            cout <<" ";
+        
+        for (int j = 1; j <= i; j++) {
+        
+            cout << "*" << " ";
         }
-        for (int j = 1; j <= i; j++)
-        {
-            cout <<"* ";
-        }
+       
         cout << endl;
     }
+    
+    for (int i = 1; i <= n - 1; i++) {
+
+      
+        for (int j = 1; j <= n - i; j++) {
+             
+            cout << "*" << " ";
+        }
+       
+        cout << endl;
+    }
+}
+
+
+int main()
+{
+
+    int n = 5;
+    
+    printRotatedTriangle(n);
     return 0;
 }
